23d655b4fa6fcd289263950138a8fb6b6c11de7c
Kyle <khumph2@gmail.com>
2019-06-22 14:46:04 -0700

    Copy estimated working weights to clipboard

--- app.R
+++ app.R
@@ -1,14 +1,34 @@
 ui <- fluidPage(
   headerPanel("Training log"),
   sidebarPanel(
-    textInput("url", "Google sheet url", "https://docs.google.com/spreadsheets/d/1bj9p-qcq6484bHZwfPVMoG8KcqEvjzXOoOJQxtwiTEc/edit#gid=1211142681"),
-    dateInput("filter_date", "Filter date", last_month),
-    selectInput("metric", "Y Variable", metrics),
-    checkboxInput("overall", "Summarize over lifts")
-  ),
+    textInput(
+      "url",
+      "URL to google sheet with your training data. Must be shared via link:",
+      "https://docs.google.com/spreadsheets/d/1bj9p-qcq6484bHZwfPVMoG8KcqEvjzXOoOJQxtwiTEc/edit#gid=1211142681"
+    ),
+    dateRangeInput("date_range", "Date range", Sys.Date() - 28, Sys.Date()),
+    selectInput("metric", "Metric", metrics),
+    checkboxInput("overall", "Summarize over lifts"),
+    checkboxInput("ind", "Plot lifts individually"),
+    "Calcuate and copy estimated working weights based off your best estimated 1RM over the given interval by clicking this button:",
+    br(), 
+    actionButton("est", "Copy weights"),
+    br(),
+    "This outputs estimated working weights for any blank",
+    code("weight_p"),
+    "rows you have. Requires values for",
+    code("rpe_p"),
+    "and",
+    code("reps_p"),
+    paste0(
+      "in each row you want to estimate, and the metric in this app to be \"",
+      names(metrics[metrics == 'e1rm']),
+      "\""
+    )
+  ), 
   mainPanel(
     textOutput("caption"),
-    plotOutput("plot1")
+    plotOutput("plot1", height = "600px")
   )
 )
 
@@ -32,7 +52,8 @@ server <- function(input, output) {
         pct1rm = round(pct_1rm(reps, rpe), 2),
         e1rm = round(weight / pct1rm)
       ) %>%
-      filter(date >= input$filter_date, !is.na(weight) | !is.na(time) | !is.na(rpe)) %>%
+      filter(date >= input$date_range[1], date <= input$date_range[2],
+             !is.na(weight) | !is.na(time) | !is.na(rpe)) %>%
       group_by(date, lift) %>%
       mutate(tonnage = weight * reps) %>%
       summarise(
@@ -78,6 +99,9 @@ server <- function(input, output) {
       theme(legend.position = "bottom") ->
       out
       
+    if (input$ind) {
+      out <- out + facet_grid(lift ~ ., scales = "free")
+    }
     if (input$overall && input$metric != "e1rm" || input$metric == "load") {
       out + theme(legend.position = "none")
     } else {
@@ -87,6 +111,23 @@ server <- function(input, output) {
   
   output$caption <- renderText({
     captions[[input$metric]]
+  })
+  
+  observeEvent(input$est, {
+    dat_raw <- rawData()
+    if (any(!is.na(dat_raw$reps_p) & !is.na(dat_raw$rpe_p) & is.na(dat_raw$weight_p))) {
+      in_raw <- dat_raw %>%
+        select(-ee1rm) %>%
+        filter(!is.na(reps_p), !is.na(rpe_p), is.na(weight_p))
+      in_e1rm <- selectedData() %>%
+        filter(param == "e1rm", lift %in% unique(in_raw$lift)) %>%
+        group_by(lift) %>%
+        summarise(ee1rm = max(value))
+      full_join(in_raw, in_e1rm, by = 'lift') %>% 
+        mutate(weight_p = ceiling(pct_1rm(reps_p, rpe_p) * ee1rm / 5) * 5) %>%
+        pull(weight_p) %>%
+        clipr::write_clip(col.names = F)
+    } 
   })
 }
 
